965c96e67c50fb12c5cbb31488aff9219ef9cc5d
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-01-20 10:10:48 +0100

    chore: Removes unused import

--- src/lib/sudoku/grid/grid.ts
+++ src/lib/sudoku/grid/grid.ts
@@ -2,7 +2,6 @@ import { isDefined } from '@/lib/utils/is-defined';
 import { IncorrectGridError, ValueOutOfRangeError } from './errors';
 import { GRID_CELLS_COUNT, GRID_SIZE, SUB_GRID_CELLS_COUNT, SUB_GRID_SIZE } from './constants';
 import type { Grid, GridCol, GridCell, GridFilled, GridRow, SubGrid } from './types';
-import { prettyDebug } from '../utils';
 
 /**
  * Fills the diagonal sub-grids (3x3 blocks) of the given grid with random digits 1-9.
