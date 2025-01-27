eea42a8aeaa7b7e4d1e193e9dd5cf11e2d163b96
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-01-27 18:55:28 +0100

    test: Improve assertions on difficulty definitons

--- src/lib/sudoku/puzzle/difficulty.spec.ts
+++ src/lib/sudoku/puzzle/difficulty.spec.ts
@@ -14,15 +14,33 @@ test.each([
 	expect(Object.keys(DifficultyLevel)).to.deep.equal(Object.keys(o));
 });
 
-test('difficulty scales correctly', () => {
-	const keys = Object.keys(TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL).map((it) => {
-		return Number.parseInt(it);
-	});
-	const easiestLevel = Math.min(...keys) as PuzzleDifficultyLevel;
-	const hardestLevel = Math.max(...keys) as PuzzleDifficultyLevel;
+test('amount of given cells scales down by difficulty', () => {
+	const levels = Object.keys(TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL)
+		.map((it) => {
+			return Number.parseInt(it);
+		})
+		.sort();
+	const easiestLevel = levels.at(0) as PuzzleDifficultyLevel;
+	const hardestLevel = levels.at(-1) as PuzzleDifficultyLevel;
 
 	expect(TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[hardestLevel][0] >= 17).to.equal(true);
 	expect(TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[easiestLevel][1] < GRID_CELLS_COUNT).to.equal(
 		true,
 	);
+
+	for (let idx = 1; idx < levels.length; idx++) {
+		const currentLevel = levels[idx] as PuzzleDifficultyLevel;
+
+		expect(
+			TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[currentLevel][0] <
+				TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[currentLevel][1],
+		).to.equal(true);
+
+		const prevLevel = levels[idx - 1] as PuzzleDifficultyLevel;
+
+		expect(
+			TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[prevLevel][1] >
+				TOTAL_GIVEN_CELLS_RANGE_BY_DIFFICULTY_LEVEL[currentLevel][0],
+		).to.equal(true);
+	}
 });
