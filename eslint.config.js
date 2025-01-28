b31c7f91d0fc9f5bb844997e5889cd5513bee235
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-01-28 14:30:45 +0100

    chore: Adds rules to test specific linter

--- eslint.config.js
+++ eslint.config.js
@@ -19,12 +19,41 @@ export default [
 	...pluginVue.configs['flat/recommended'],
 	...pluginVue.configs['flat/strongly-recommended'],
 	...vueTsEslintConfig({
-		extends: ['recommended', 'eslintRecommended', 'strictTypeChecked'],
+		extends: [
+			'recommended',
+			'eslintRecommended',
+			'recommendedTypeChecked',
+			'stylisticTypeChecked',
+			'strictTypeChecked',
+		],
 	}),
 
 	{
+		files: ['src/**/__tests__/*', 'src/**/*.spec.*'],
 		...pluginVitest.configs.recommended,
-		files: ['src/**/__tests__/*'],
+		rules: {
+			...pluginVitest.configs.recommended.rules,
+			'vitest/valid-expect': 'off', // TODO: Does not recognize chai
+			'vitest/valid-title': 'off', // TODO: Does not recognize Function.name
+			'vitest/require-hook': 'error',
+			'vitest/prefer-spy-on': 'warn',
+			'vitest/prefer-mock-promise-shorthand': 'warn',
+			'vitest/prefer-hooks-in-order': 'warn',
+			'vitest/prefer-each': 'warn',
+			'vitest/prefer-called-with': 'warn',
+			'vitest/padding-around-all': 'warn',
+			'vitest/no-test-return-statement': 'warn',
+			'vitest/no-test-prefixes': 'warn',
+			'vitest/no-standalone-expect': 'error', // Expects should be within test blocks
+			'vitest/no-interpolation-in-snapshots': 'error', // Can cause unstable snapshots
+			'vitest/no-focused-tests': 'error',
+			'vitest/no-duplicate-hooks': 'error', // Indicates confusing/redundant setup
+			'vitest/no-disabled-tests': 'error', // Prevents accumulating "dead" test code
+			'vitest/no-conditional-tests': 'error', // Tests should be deterministic
+			'vitest/no-conditional-expect': 'error', // Expectations should be consistent
+			'vitest/no-commented-out-tests': 'error',
+			'vitest/max-nested-describe': 'error',
+		},
 	},
 
 	oxlint.configs['flat/recommended'],
