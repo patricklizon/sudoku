750432732ce7ad81d5b633af0110198ca4939196
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-02-28 04:24:43 +0100

    chore: prepare view for poc

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from 'vue';
+
+export const App = defineComponent({
+	render() {
+		return <div>hello</div>;
+	},
+});
--- src/views/home-view.tsx
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue';
-
-export const HomeView = defineComponent({
-	render() {
-		return <main>hello universe</main>;
-	},
-});
-
-export default HomeView;
