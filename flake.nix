4cb6cbe475a3fa76da7f5e8341553d2d8960f163
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-04-07 03:13:26 +0200

    fixup

--- flake.nix
+++ flake.nix
@@ -1,25 +1,48 @@
 {
-  description = "Node.js project";
+  description = "Node.js project with Playwright";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixd.url = "github:nix-community/nixd";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixd }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         nodejs = pkgs.nodejs_22;
+        playWrightBrowserDeps = with pkgs; [
+          xvfb-run
+          libX11
+          libXcomposite
+          libXcursor
+          libXdamage
+          libXext
+          libXi
+          libXtst
+          libXrandr
+          libXScrnSaver
+          libxshmfence
+          libGLU
+          mesa
+          nss
+          nspr
+          fontconfig
+          freetype
+        ];
+
       in {
         formatter = pkgs.nixfmt;
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [ nodejs ];
+          buildInputs = with pkgs; [ nodejs playwright-test ] ++ playWrightBrowserDeps;
 
           shellHook = ''
             export HOME=$(mktemp -d)
             export npm_config_cache=$HOME/.npm
+            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
 
             echo "Node.js $(node --version)"
             echo "npm $(npm --version)"
