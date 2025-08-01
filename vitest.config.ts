f022f5314b06ca588469b3235d815a818528b005
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-01 17:56:47 +0200

    add ts config paths plugin to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { fileURLToPath } from "node:url";
 import { configDefaults, defineConfig } from "vitest/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	test: {
 		exclude: [...configDefaults.exclude, "tests/**"],
 		root: fileURLToPath(new URL("./", import.meta.url)),
 	},
+	plugins: [tsConfigPaths()],
 });
