2d31f1de98dbc136142f2a9293281d4ceb9d4e89
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-01 14:44:30 +0200

    fixup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const baseURL = 'http://localhost:8787';
+const baseURL = 'http://localhost:3000';
 
 export default defineConfig({
 	testDir: './tests',
