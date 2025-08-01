2c186fba3ff79f5a5d4107dcc285e4b51db7021f
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-01 17:53:15 +0200

    update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,9 @@
-import { fileURLToPath } from 'node:url';
-import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import { fileURLToPath } from "node:url";
+import { configDefaults, defineConfig } from "vitest/config";
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			exclude: [...configDefaults.exclude, 'e2e/**'],
-			root: fileURLToPath(new URL('./', import.meta.url)),
-		},
-	}),
-);
+export default defineConfig({
+	test: {
+		exclude: [...configDefaults.exclude, "e2e/**"],
+		root: fileURLToPath(new URL("./", import.meta.url)),
+	},
+});
