10954e84a4a3392071802dc4a3a1e427cedaef40
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-02 22:28:51 +0200

    fixup

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,7 +1,7 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
-const rootElement = document.getElementById("app");
+const rootElement = document.querySelector("#app");
 if (!rootElement) throw new Error("node for mounting the app does not exist");
 
 mount(() => <StartClient />, rootElement);
--- src/features/deployment-info/component.tsx
+++ src/features/deployment-info/component.tsx
@@ -1,11 +1,15 @@
 import type { JSX } from "solid-js";
-import { getDeploymentInfoQuery } from "#src/lib/queries/deployment";
+import { getRequestEvent } from "solid-js/web";
+import type { DeploymentInfo } from "#src/lib/domain/deployment";
+import { isEmpty } from "#src/lib/utils/is-empty";
+import { isNil } from "#src/lib/utils/is-nil";
+import type { Option } from "#src/lib/utils/types/option";
 
 /**
  * Renders details about the environment in which the application is currently deployed.
  */
 export function DeploymentInfo(): JSX.Element {
-	const deploymentInfo = getDeploymentInfoQuery();
+	const deploymentInfo = getDeploymentInfo();
 
 	return (
 		<footer>
@@ -13,4 +17,27 @@ export function DeploymentInfo(): JSX.Element {
 			<p>Deploy URL: {deploymentInfo.url}</p>
 		</footer>
 	);
+}
+
+type DeploymentInfoQueryResult = {
+	id: Option<DeploymentInfo["id"]>;
+	url: Option<DeploymentInfo["url"]>;
+};
+
+/**
+ * Handles GET requests to retrieve deployment information.
+ * This function extracts deployment data from the Cloudflare environment
+ * available through the SolidStart request event.
+ */
+function getDeploymentInfo(): DeploymentInfoQueryResult {
+	"use server";
+
+	const event = getRequestEvent();
+	if (isNil(event)) throw new Error("No request context");
+
+	const env = event.nativeEvent.context.cloudflare.env;
+	return {
+		id: env.DEPLOYMENT_ID,
+		url: isEmpty(env.DEPLOY_URL) ? event.request.url : env.DEPLOY_URL,
+	};
 }
