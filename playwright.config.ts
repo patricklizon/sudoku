4b6caa9a089dc5bd2a66d8c9131036edc489ad15
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-03 03:02:34 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 const baseURL = "http://localhost:3000";
 
 export default defineConfig({
-	testDir: "./tests",
+	testDir: "./e2e",
 	fullyParallel: !process.env.CI,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
