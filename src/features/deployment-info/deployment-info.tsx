a2f1919d5b67074402519cecf73ae2ce17279f13
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-08-04 02:33:17 +0200

    fix: Display deployment time in user timezone by rendering it on client side

--- src/features/deployment-info/deployment-info.tsx
+++ src/features/deployment-info/deployment-info.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "solid-js";
+import { createSignal, type JSX, onMount } from "solid-js";
 import { useDeploymentInfo } from "#src/context/deployment";
 import type { DeploymentInfo } from "#src/lib/domain/deployment";
 import { isEmpty } from "#src/lib/utils/is-empty";
@@ -10,18 +10,23 @@ import { isNil } from "#src/lib/utils/is-nil";
 export function DeploymentInfo(): JSX.Element {
 	const info = useDeploymentInfo();
 
-	const formattedTimestamp =
-		isNil(info.timestamp) || isEmpty(info.timestamp)
-			? null
-			: new Intl.DateTimeFormat(undefined, {
-					dateStyle: "medium",
-					timeStyle: "medium",
-				}).format(new Date(info.timestamp));
+	const [formattedTimestamp, setFormattedTimestamp] = createSignal<Option<string>>();
+
+	onMount(function formatTimeInLocalTimeZone() {
+		if (isNil(info.timestamp) || isEmpty(info.timestamp)) return;
+
+		setFormattedTimestamp(
+			new Intl.DateTimeFormat(undefined, {
+				dateStyle: "medium",
+				timeStyle: "medium",
+			}).format(new Date(info.timestamp)),
+		);
+	});
 
 	return (
 		<footer>
 			{isNil(info.id) ? null : <p>Deployment ID: {info.id}</p>}
-			{isNil(formattedTimestamp) ? null : <p>Deployed At: {formattedTimestamp}</p>}
+			{isNil(formattedTimestamp) ? null : <p>Deployed At: {formattedTimestamp()}</p>}
 			{isNil(info.host) ? null : <p>Deploy URL: {info.host}</p>}
 			{isNil(info.pullRequestURL) ? null : (
 				<p>
